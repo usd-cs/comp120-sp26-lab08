7747e6e61e52d2f4d8f90775a2d4b44d84fa0998
Sat Garcia <sat@sandiego.edu>
2026-03-19 12:06:05 -0700

    changed order of comparison

--- sorting.py
+++ sorting.py
@@ -33,7 +33,7 @@ def insertion_sort(arr: list[int]) -> None:
 
         # Move elements of arr[0..i-1] that are greater than key
         # to one position ahead of their current position
-        while j >= 0 and arr[j] > key:
+        while j >= 0 and key < arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
 
