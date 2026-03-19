6c63094f4b253961af4123c81d06b613e41cecae
Sat Garcia <sat@sandiego.edu>
2026-03-18 18:22:57 -0700

    add sorting algorithms and the pytest functions

---
+++ sorting.py
@@ -0,0 +1,41 @@
+"""
+Module: sorting
+
+Implementation of some (non-recursive) sorting algorithms.
+"""
+
+def selection_sort(arr: list[int]) -> None:
+    """
+    Uses selection sort to sort the given list of integers (arr).
+    """
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        # Swap only if a smaller element was found
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+def insertion_sort(arr: list[int]) -> None:
+    """
+    Uses insertion sort to sort the given list of integers (arr).
+    """
+
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        # Move elements of arr[0..i-1] that are greater than key
+        # to one position ahead of their current position
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        # Place the key in its correct location
+        arr[j + 1] = key
---
+++ test_sorting.py
@@ -0,0 +1,34 @@
+from sorting import selection_sort, insertion_sort
+
+def test_selection_sort_int():
+    """ Tests selection_sort on lists of integers. """
+    empty_list = []
+    selection_sort(empty_list)
+    assert len(empty_list) == 0
+
+    single_item_list = [5]
+    selection_sort(single_item_list)
+    assert single_item_list == [5]
+
+    multi_item_list = [5, 3, 1]
+    selection_sort(multi_item_list)
+    assert multi_item_list == [1, 3, 5]
+
+    multi_item_list_neg = [5, -2, 1]
+    selection_sort(multi_item_list_neg )
+    assert multi_item_list_neg == [-2, 1, 5]
+
+
+def test_insertion_sort_int():
+    """ Tests insertion_sort on lists of integers. """
+    test_cases = [
+        ([], []),
+        ([5], [5]),
+        ([5, 3, 1], [1, 3, 5]),
+        ([5, -2, 1], [-2, 1, 5])
+    ]
+
+    for pre, post in test_cases:
+        l = pre
+        insertion_sort(l)
+        assert l == post
